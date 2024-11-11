f6b3a668e532fe6c5dd9315264769a0d28591263
TERUZ-VTAT <terunori.kato0911@gmail.com>
2024-11-11 14:28:27 +0900

    5

--- search_project/build_files.sh
+++ search_project/build_files.sh
@@ -1,5 +1,5 @@
-pip3 install pipenv
-python3.9 -m pipenv sync
+python -m pip install pipenv
+python -m pipenv sync
 # pip3 install -r requirements.txt
-python3.9 -m pipenv shell
+python -m pipenv shell
 python manage.py collectstatic
