e88d7bb516a89c28241c36b201c5fcdb39697b33
TERUZ-VTAT <terunori.kato0911@gmail.com>
2024-12-02 13:57:02 +0900

    db

--- search_project/search_project/settings.py
+++ search_project/search_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$at%ntj!g62kye)i-sl96$)am3_02*f-ou*g0zo7mx*9qn*+6^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
