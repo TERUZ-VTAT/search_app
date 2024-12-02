f33bb21355a239b21a8c3c5e414d4ec6394ec294
TERUZ-VTAT <terunori.kato0911@gmail.com>
2024-12-02 13:42:32 +0900

    last?

--- search_project/build_files.sh
+++
@@ -1,7 +0,0 @@
-# python3.12 -m pip install pipenv
-# python3.12 -m venv env
-sudo apt-get install libsqlite3-dev libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev libssl-dev tcl-dev tk-dev libreadline-dev
-python3.12 -m pip install -r requirements.txt
-# python3.12 -m pipenv shell
-# source env/bin/activate
-python3.12 manage.py collectstatic
